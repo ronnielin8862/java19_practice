16560ecbb66a44467cf4e3427dc01e8647c4a201
ronnie <ronnielin8862@gmail.com>
2024-04-10 11:02:18 +0800

    record how to get a construct class from spring context

--- src/main/java/org/example/freePractice/construct/Construct.java
+++ src/main/java/org/example/freePractice/construct/Construct.java
@@ -1,12 +1,24 @@
 package org.example.freePractice.construct;
 
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
 @Lazy
+@Scope("prototype")
 public class Construct {
-    private String flag = "";
+//    private ThreadLocal<String> flag = new ThreadLocal<>(); // todo threadlocal
+//    public Construct(String flag){
+//        this.flag.set(flag);
+//        System.out.println("new flag = " + flag);
+//    }
+//
+//    public String getFlag(){
+//        return flag.get();
+//    }
+
+    private String flag;
 
     public Construct(String flag){
         this.flag = flag;
--- src/test/java/org/example/freePractice/getBeanByConstruct/GetBeanByConstruct.java
+++ src/test/java/org/example/freePractice/getBeanByConstruct/GetBeanByConstruct.java
@@ -1,11 +1,11 @@
 package org.example.freePractice.getBeanByConstruct;
 
 import org.example.freePractice.construct.Construct;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
 
 @SpringBootTest
 public class GetBeanByConstruct {
@@ -14,10 +14,48 @@ public class GetBeanByConstruct {
     BeanFactory beanFactory;
 
     @Test
+    public void testMultiThreadGetConstruct() throws InterruptedException {
+
+        int times = 100;
+        new Thread(()->{
+            String flag = "A";
+            Construct construct = beanFactory.getBean(Construct.class,flag);
+            for (int i = 0; i<= times; i++){
+                String returnFlag = construct.getFlag();
+                System.out.println("A thread flag = "+returnFlag);
+                Assert.assertEquals("NOT EQUAL",returnFlag,flag);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        new Thread(()->{
+            String flag = "B";
+            Construct construct = beanFactory.getBean(Construct.class,flag);
+            for (int i = 0; i<= times; i++){
+                String returnFlag = construct.getFlag();
+                System.out.println("B thread flag = "+returnFlag);
+                Assert.assertEquals("NOT EQUAL",returnFlag, "AAAA");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        Thread.sleep(2000);
+        System.out.println("Finish");
+    }
+
+    @Test
     public void testGetConstruct(){
         String flag = "A";
         Construct construct = beanFactory.getBean(Construct.class,flag);
-
-        System.out.println("flag = "+construct.getFlag());
+        System.out.println("get = " + construct.getFlag());
     }
+
 }
